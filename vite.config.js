e6b998f84589645ce706f779b948664dbaa6725d
raman <raman.risaanva@2wls.net>
2025-01-08 19:03:09 +0530

    update vite version

--- vite.config.js
+++ vite.config.js
@@ -7,13 +7,12 @@
 // })
 
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',  // Ensure the build output goes to the 'dist' folder
+    outDir: 'dist',
   },
-})
+});
